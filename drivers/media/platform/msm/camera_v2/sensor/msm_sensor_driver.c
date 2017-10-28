84c06c275f14cdee4c44e86b14251f76e8476598
harry8242 <mohamedharis97@gmail.com>
2017-10-28 07:08:27 +0000

    Fix build

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -958,17 +958,14 @@ int32_t msm_sensor_driver_probe(void *setting,
 
 	msm_sensor_fill_sensor_info(s_ctrl, probed_info, entity_name);
 
-<<<<<<< HEAD
 	/*
 	 * Set probe succeeded flag to 1 so that no other camera shall
 	 * probed on this slot
 	 */
 	s_ctrl->is_probe_succeed = 1;
-=======
 	hardwareinfo_set_prop(probed_info->position==BACK_CAMERA_B
 		?HARDWARE_BACK_CAM:HARDWARE_FRONT_CAM,probed_info->sensor_name);
 
->>>>>>> msm: camera_v2: Import camera changes
 	return rc;
 
 camera_power_down:
